46c4acf66726433a9843dd418a73ae16c6442abc
Gabriel Landim <55588010+Glandim@users.noreply.github.com>
2020-11-10 00:50:42 -0300

    Revert "Segunda entrega"

--- src/controller/table.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-tableBlueprint = Blueprint('table', __name__, url_prefix='/table')
-
-@tableBlueprint.route('/')
-def tableIndex():
-  return render_template('table/table.html', title='Tabela das incidências')
-  
--- src/model/disease.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Disease(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(200), unique=True, nullable=False)
-    symptoms = db.Column(db.String(1000), nullable=False) 
-    
